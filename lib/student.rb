c4583bf5b98635c85b6326d3a443336b8987bc4a
Monica R <mlisareyes@gmail.com>
2019-11-06 19:52:29 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -67,7 +67,11 @@ class Student
       SELECT *
       FROM students
       WHERE name = ?
-      end
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end
   end
 
 end
